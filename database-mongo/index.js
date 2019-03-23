c986aa2078385a07dffde99a20549100454b2b22
sveta0112 <sveta0112@hotmail.com>
2019-03-23 14:55:59 -0700

    4changes

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //const mongoURI = 'mongodb://localhost:27017/nanny';
 
 
-const mongoURI = 'mongodb://lana:lana1234@ds019866.mlab.com:19866/nanny';
+const mongoURI = process.env.MONGODB_URI || 'mongodb://lana:lana1234@ds019866.mlab.com:19866/nanny';
 
 const db = mongoose.connect(mongoURI, { useNewUrlParser: true });
 
